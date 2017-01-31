076b06e9498b4c3478cd4eddeafc27acfb28efcc
sophiedherbomez <sophie.dherbomez@insead.edu>
2017-02-01 01:02:33 +0800

    updates

---
+++ libraries.R
@@ -0,0 +1,9 @@
+library(pyramid)
+library(plyr)
+library(ggplot2)
+
+#Visualization function for the various tables
+visu<-function(x,l){
+  rownames(x) <- paste0("line ", sprintf("%02i", 1:nrow(x)))
+  knitr::kable(x[1:min(l,nrow(x)), ])
+}
