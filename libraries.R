7ef7f47a8a82f7f57eb3a25443ad3e22ebe4cf5c
sophiedherbomez <sophie.dherbomez@insead.edu>
2017-02-01 09:45:40 +0800

    update

--- libraries.R
+++ libraries.R
@@ -4,6 +4,6 @@ library(ggplot2)
 
 #Visualization function for the various tables
 visu<-function(x,l){
-  rownames(x) <- paste0("line ", sprintf("%02i", 1:nrow(x)))
+  rownames(x) <- paste0(sprintf("%02i", 1:nrow(x)))
   knitr::kable(x[1:min(l,nrow(x)), ])
 }
